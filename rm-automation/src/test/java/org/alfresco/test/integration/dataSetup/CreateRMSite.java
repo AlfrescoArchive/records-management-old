0bf141c9b25066c3be581613316a60f3d6cc9d71
dwebster <dwebster@7033f55c-6c29-4ad8-afba-765cb616e738>
2015-04-13 09:31:07 +0000

    RM-2081: UI Integration test data setup for RM site and file plan
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/modules/recordsmanagement/HEAD@101735 7033f55c-6c29-4ad8-afba-765cb616e738

---
+++ rm-automation/src/test/java/org/alfresco/test/integration/dataSetup/CreateRMSite.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (C) 2005-2014 Alfresco Software Limited.
+ *
+ * This file is part of Alfresco
+ *
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.alfresco.test.integration.dataSetup;
+
+import org.alfresco.po.rm.site.RMSiteDashboard;
+import org.alfresco.po.share.site.create.CreateSiteDialog;
+import org.alfresco.po.share.site.create.SiteType;
+import org.alfresco.po.share.userdashboard.dashlet.MySitesDashlet;
+import org.alfresco.test.BaseTest;
+import org.junit.Assert;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.Test;
+
+/**
+ * Create RM site
+ *
+ * @author Tuna Aksoy, David Webster
+ * @since 3.0
+ */
+public class CreateRMSite extends BaseTest
+{
+    /** rm site dashboard */
+    @Autowired
+    private RMSiteDashboard rmSiteDashboard;
+    
+    /** my sites dashlet */
+    @Autowired
+    private MySitesDashlet mySitesDashlet;
+    
+    /** create site dialog */
+    @Autowired
+    private CreateSiteDialog createSiteDialog;
+    
+    /**
+     * Create RM site for Integration tests
+     */
+    @Test
+            (
+                    groups = {"integration-dataSetup-rmSite", "integration-dataSetup-all"},
+                    description = "Create RM Site"
+            )
+    public void createRMSite()
+    {     
+    	// create RM site
+        openPage(userDashboardPage);
+
+        if (!mySitesDashlet.siteExists(RM_SITE_ID))
+        {
+            // create RM site
+            mySitesDashlet
+                    .clickOnCreateSite()
+                    .setSiteType(SiteType.RM_SITE)
+                    .clickOnOk();
+
+            // navigate back to the user dashboard
+            openPage(userDashboardPage);
+        }
+        // ensure the rm site exists
+        Assert.assertTrue(mySitesDashlet.siteExists(RM_SITE_ID));
+
+        // enter the rm site via the my sites dashlet
+        mySitesDashlet.clickOnRMSite(RM_SITE_ID);
+    }
+    
+
+    @AfterSuite(alwaysRun=true)
+    protected void deleteRMSite()
+    {     
+        openPage(userDashboardPage);
+        
+        // check for existence of site
+        if (mySitesDashlet.siteExists(RM_SITE_ID))
+        {       
+            // delete site
+            mySitesDashlet.clickOnDeleteSite(RM_SITE_ID);
+            Assert.assertFalse(mySitesDashlet.siteExists(RM_SITE_ID));
+        }
+    }
+}
