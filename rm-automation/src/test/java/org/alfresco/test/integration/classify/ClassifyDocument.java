8e57cbe5b188540188728a28d6eeddf60dfd8484
dwebster <dwebster@7033f55c-6c29-4ad8-afba-765cb616e738>
2015-04-13 09:32:58 +0000

    WIP: RM-2079: Test for Classify Document action
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/modules/recordsmanagement/HEAD@101737 7033f55c-6c29-4ad8-afba-765cb616e738

---
+++ rm-automation/src/test/java/org/alfresco/test/integration/classify/ClassifyDocument.java
@@ -0,0 +1,55 @@
+package org.alfresco.test.integration.classify;
+
+import org.alfresco.po.share.browse.documentlibrary.DocumentActions;
+import org.alfresco.po.share.browse.documentlibrary.DocumentLibrary;
+import org.alfresco.po.share.details.document.DocumentActionsPanel;
+import org.alfresco.po.share.details.document.DocumentDetails;
+import org.alfresco.test.BaseTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.testng.annotations.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Classify document integration test
+ *
+ * @author David Webster
+ * @since 3.0
+ */
+public class ClassifyDocument extends BaseTest {
+    /**
+     * Doc Lib
+     */
+    @Autowired
+    private DocumentLibrary documentLibrary;
+
+    /**
+     * Doc Details page
+     */
+    @Autowired
+    private DocumentDetails documentDetails;
+
+    /**
+     * Main test execution
+     */
+    @Test
+            (
+                    groups = {"integration"},
+                    description = "Verify Classify Document behaviour",
+                    dependsOnGroups = {"integration-dataSetup-collab"}
+            )
+    public void classifyDocument() {
+        // Open Collab site DocumentLibrary.
+        openPage(documentLibrary, COLLAB_SITE_ID, "documentlibrary");
+
+        // verify document actions
+        assertTrue(documentLibrary.getDocument(DOCUMENT).isActionClickable(DocumentActions.CLASSIFY));
+
+        // navigate to the document details page
+        // FIXME: this breaks...
+        documentLibrary.getDocument(DOCUMENT).clickOnLink();
+
+        // verify that all the expected actions are available
+        assertTrue(documentDetails.getDocumentActionsPanel().isActionClickable(DocumentActionsPanel.CLASSIFY));
+    }
+}
