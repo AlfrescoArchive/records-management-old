3af80888aca0f85aadc3cffff6abc2e5e652408a
dwebster <dwebster@7033f55c-6c29-4ad8-afba-765cb616e738>
2015-04-22 12:05:42 +0000

    RM-2066: Adds Indicators interface
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/modules/recordsmanagement/HEAD@102297 7033f55c-6c29-4ad8-afba-765cb616e738

---
+++ rm-automation/src/main/java/org/alfresco/po/rm/browse/fileplan/Indicators.java
@@ -0,0 +1,11 @@
+package org.alfresco.po.rm.browse.fileplan;
+
+/**
+ * Indicators common for all items
+ * @author David Webster
+ */
+public interface Indicators
+{
+    static final String EXIF = "exif";
+    static final String GEOGRAPHIC = "geographic";
+}
