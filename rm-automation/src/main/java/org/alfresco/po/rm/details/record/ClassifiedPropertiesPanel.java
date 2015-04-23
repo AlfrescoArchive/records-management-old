4e97ea5bcd16ff6a2ee759eac0066552c4f2706c
dwebster <dwebster@7033f55c-6c29-4ad8-afba-765cb616e738>
2015-04-23 10:31:16 +0000

    RM-2063: WIP: Add classified document properties page objects and tests
    git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/modules/recordsmanagement/HEAD@102385 7033f55c-6c29-4ad8-afba-765cb616e738

---
+++ rm-automation/src/main/java/org/alfresco/po/rm/details/record/ClassifiedPropertiesPanel.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2005-2015 Alfresco Software Limited.
+ *
+ * This file is part of Alfresco
+ *
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.alfresco.po.rm.details.record;
+
+import org.alfresco.po.common.annotations.WaitFor;
+import org.alfresco.po.common.annotations.WaitForStatus;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class ClassifiedPropertiesPanel extends PropertiesPanel
+{
+    @FindBy(css="div.document-metadata-header h2")
+    private WebElement classifiedTitle;
+
+    @FindBy(css="div.document-details-panel .set-panel .viewmode-value")
+    private List<WebElement> classifiedProperties;
+
+    public static Integer ORIGINAL_CLASSIFICATION = 0;
+    public static Integer CURRENT_CLASSIFICATION = 1;
+    public static Integer CLASSIFICATION_AUTHORITY = 2;
+    public static Integer CLASSIFICATION_REASON = 3;
+
+    public String getClassifiedProperty(Integer property)
+    {
+        return classifiedProperties.get(property).getText();
+    }
+}
---
+++ rm-automation/src/test/java/org/alfresco/test/integration/classify/ClassifiedDocumentProperties.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2005-2015 Alfresco Software Limited.
+ *
+ * This file is part of Alfresco
+ *
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.alfresco.test.integration.classify;
+
+import org.alfresco.po.rm.details.record.ClassifiedPropertiesPanel;
+import org.alfresco.po.share.browse.documentlibrary.DocumentLibrary;
+import org.alfresco.po.share.details.document.ClassifiedDocumentDetails;
+import org.alfresco.test.BaseTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.testng.annotations.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Classify document integration test
+ *
+ * @author David Webster
+ * @since 3.0
+ */
+public class ClassifiedDocumentProperties extends BaseTest
+{
+    /**
+     * Doc Lib
+     */
+    @Autowired
+    private DocumentLibrary documentLibrary;
+
+    @Autowired
+    private ClassifiedDocumentDetails classifiedDocumentDetails;
+
+    @Autowired
+    private ClassifiedPropertiesPanel classifiedPropertiesPanel;
+    /**
+     * Main test execution
+     */
+    @Test(
+        groups = { "integration" },
+        description = "Verify Classify Document behaviour"
+        // FIXME: Add dataSetup method here, once work for RM-2051 is complete (e.g. req. classify content dialog PO)
+        // ,
+        // dependsOnGroups = { "integration-dataSetup-collab-classified" }
+    )
+
+    public void classifyDocumentIndicator()
+    {
+        // Open Collab site DocumentLibrary.
+        openPage(documentLibrary, COLLAB_SITE_ID, "documentlibrary");
+
+        // navigate to the document details page
+        documentLibrary.getDocument(CLASSIFIED_DOCUMENT)
+            .clickOnLink(classifiedDocumentDetails);
+
+        // verify that classification is as expected.
+        assertTrue(classifiedPropertiesPanel.getClassifiedProperty(ClassifiedPropertiesPanel.CURRENT_CLASSIFICATION)
+            .equals(CLASSIFIED_DOCUMENT_CLASSIFICATION));
+    }
+}
